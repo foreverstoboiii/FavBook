fa1a2621fe87a3ba7c7118ceffdfcba8506dc6a4
foreverstoboiii <shukurullayevnurmukhammad123@gmail.com>
2025-07-28 13:46:57 +0500

    Working on server. Rough version.

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const http = require('http')
+const fs = require('fs')
 
-const server = http.createServer((req, res ) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('HELLOSERVER!');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    // res.end('HELLOSERVER!');
+
+    if (req.url == '/')
+        fs.createReadStream('./index.html').pipe(res)
+    else if (req.url == '/about')
+        fs.createReadStream('./html templates/about.html').pipe(res)
+    else
+        fs.createReadStream('./html templates/error.html').pipe(res)
 })
 
 const PORT = 3000;
 const HOST = 'localhost';
 
-server.listen(PORT, HOST, ()=> {
+server.listen(PORT, HOST, () => {
     console.log(`Server is running at: http://${HOST}:${PORT}`);
-    
+
 })
