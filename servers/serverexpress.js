e86e437f575d36e576cec0586fd2cb32fbb9f96f
foreverstoboiii <shukurullayevnurmukhammad123@gmail.com>
2025-08-06 14:00:20 +0500

    Remove unnecessary blank line in serverexpress.js

--- servers/serverexpress.js
+++ servers/serverexpress.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
 const app = express();
 const PORT = 3000;
 const dbpath = path.join(__dirname, '../db.json');
